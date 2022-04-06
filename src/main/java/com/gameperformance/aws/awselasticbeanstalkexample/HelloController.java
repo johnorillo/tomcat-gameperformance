1a292382e80e7277b0117fd25e008de3ea2055da
j.orillo <j.orillo@samsung.com>
2022-04-06 22:21:09 +0800

    refactored hello controller

--- src/main/java/com/gameperformance/aws/awselasticbeanstalkexample/HelloController.java
+++ src/main/java/com/gameperformance/aws/awselasticbeanstalkexample/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping
     public String graph() {
-        return "Game Performance Graph ";
+        return "Game Performance Graph New Commit ";
     }
 }
