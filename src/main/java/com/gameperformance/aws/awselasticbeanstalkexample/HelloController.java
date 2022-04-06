6092ab0da26c9c551f2fac3c4d6ac3008097b51b
j.orillo <j.orillo@samsung.com>
2022-04-06 15:35:49 +0800

    tomcat gameperf intial commit

---
+++ src/main/java/com/gameperformance/aws/awselasticbeanstalkexample/HelloController.java
@@ -0,0 +1,15 @@
+package com.gameperformance.aws.awselasticbeanstalkexample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/graph")
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public String graph() {
+        return "Game Performance Graph ";
+    }
+}
